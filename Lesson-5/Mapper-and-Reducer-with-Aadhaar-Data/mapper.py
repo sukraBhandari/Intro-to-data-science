7a6f7f549d7d43344e7c7eb5ed029fd301b39116
sukraBhandari <sukrab@yahoo.com>
2014-11-02 09:25:25 -0800

    Update mapper.py

--- Lesson-5/Mapper-and-Reducer-with-Aadhaar-Data/mapper.py
+++ Lesson-5/Mapper-and-Reducer-with-Aadhaar-Data/mapper.py
@@ -20,32 +20,3 @@ def mapper():
         print "{0}\t{1}".format(data[3],data[8])
             
 mapper()
-#######################################################
-#aadhaar_genearated_reducer.py
-
-def reducer():
-
-    aadhaar_generated = 0
-    old_key = None
-
-    #Cycle through the list of key-value pairs emitted
-    #by your mapper, and print out each key once,
-    #along with the total number of Aadhaar generated,
-    #separated by a tab.  Assume that the list of key-
-    #value pairs will be ordered by key.  Make sure
-    #each key-value pair is formatted correctly!
-    #Here's a sample final key-value pair: "Gujarat\t5.0"
-    for line in sys.stdin:
-        data = line.strip().split("\t")
-        if len(data) != 2:
-            continue
-        thisKey, thisGen = data
-        if old_key and old_key != thisKey:
-            print "{0}\t{1}".format(old_key, aadhaar_generated)
-            aadhaar_generated = 0
-        old_key = thisKey
-        aadhaar_generated += float( thisGen)
-    if old_key != None:
-        print "{0}\t{1}".format(old_key, aadhaar_generated)
-        
-reducer()
